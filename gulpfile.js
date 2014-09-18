72493de946e2d6f2e599cfb9f9e36dca97d1b4ec
Chris DeLuca <bronzehedwick@gmail.com>
2014-09-18 14:33:26 -0400

    Add gulp task for sass compilation.

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,30 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var compass = require('gulp-compass');
 
-// Minify images in the specified bucket
+// Compress scripts
 gulp.task('compress', function() {
   'use strict';
 
-  // Minify the images
-  return gulp.src('scripts/src/*.js')
+  return gulp.src('./scripts/src/*.js')
     .pipe(uglify())
     .pipe(rename('main.min.js'))
-    .pipe(gulp.dest('scripts/build'));
+    .pipe(gulp.dest('./scripts/build'));
+});
+
+// Compile SASS
+gulp.task('sass', function() {
+  'use strict';
+
+  return gulp.src('./styles/sass/*.scss')
+    .pipe(compass({
+      config_file: './styles/config.rb',
+      css: './styles/css',
+      sass: './style/sass'
+    }))
+    .pipe(gulp.dest('./styles/css'));
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['compress']);
+gulp.task('default', ['compress', 'sass']);
