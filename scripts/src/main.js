86d8ebe693ca5f56c471d86f3f5f0215b2899dc5
Chris DeLuca <bronzehedwick@gmail.com>
2014-09-17 21:07:51 -0400

    Add interactive day and night cycle.
    How cool is that?

--- scripts/src/main.js
+++ scripts/src/main.js
@@ -1,16 +1,34 @@
 (function() {
   'use strict';
 
-  var sun = document.getElementById('sun');
+  // Setup variables.
+  var sun = document.getElementById('sun'),
+      hillBg = document.getElementById('hill-background'),
+      hillFg = document.getElementById('hill-foreground'),
+      sky = document.getElementById('sky');
 
+  // Mouse move event listener.
   document.addEventListener('mousemove', function(e) {
     var top = e.clientY - 100,
-        left = e.clientX - 100;
+        left = e.clientX - 100,
+        aboveTheHills;
 
+    // Stick the sun to the cursor.
     sun.setAttribute('style', 'left:' + left + 'px;top:' + top + 'px');
-  }, false);
 
-  sun.addEventListener('click', function(e) {
-    sun.className = 'sun clicked';
+    // Detect night.
+    aboveTheHills = window.innerHeight - hillBg.clientHeight + (sun.clientHeight / 2);
+    if (e.clientY > aboveTheHills) {
+      // It's night.
+      sky.className = 'sky night';
+      hillBg.className = 'hill-background night';
+      hillFg.className = 'hill-foreground night';
+    }
+    else {
+      // It's day.
+      sky.className = 'sky';
+      hillBg.className = 'hill-background';
+      hillFg.className = 'hill-foreground';
+    }
   }, false);
 })();
