46144259ec3be3f23c432ef2300777ae43ccf893
Chris DeLuca <bronzehedwick@gmail.com>
2014-09-17 20:34:19 -0400

    Improve javascript formatting.

--- scripts/src/main.js
+++ scripts/src/main.js
@@ -6,11 +6,11 @@
   document.addEventListener('mousemove', function(e) {
     var top = e.clientY - 100,
         left = e.clientX - 100;
+
     sun.setAttribute('style', 'left:' + left + 'px;top:' + top + 'px');
   }, false);
 
   sun.addEventListener('click', function(e) {
     sun.className = 'sun clicked';
   }, false);
-
 })();
